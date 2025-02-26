5a3c1c6eae466cd166412874795c08b2a4d30c93
VinceNeede <v.bisogno3@studenti.unipi.it>
2025-02-26 22:05:14 +0100

    Added keyword inplace and created macro @Combine!

--- NDTensors/src/combiner/contract.jl
+++ NDTensors/src/combiner/contract.jl
@@ -1,14 +1,46 @@
-function contraction_output(
-  ::TensorT1, ::TensorT2, indsR::Tuple
-) where {TensorT1<:CombinerTensor,TensorT2<:DenseTensor}
-  TensorR = contraction_output_type(TensorT1, TensorT2, indsR)
-  return similar(TensorR, indsR)
+"""
+  @Combine! combiner * dense_tensor
+  
+  A macro that allows to contract a `CombinerTensor`` with a `DenseTensor` in place.
+  The reseult is a `DenseTensor` that shares the storage of the dense_tensor.
+
+  # Example
+  ```julia
+  i,j,k = Index.((2,2,3))
+  A = random_itensor(i,j,k)
+  C = combiner(i,j)
+  B = @Combine! C*A
+  @show storage(B) === storage(A) # true
+  ```
+"""
+macro Combine!(ex)
+  if (ex.head != :call) || (length(ex.args) != 3) || ex.args[1] != :*
+    error("Expected @Combine! combiner * dense_tensor")
+  end
+  T1 = esc(ex.args[2])
+  T2 = esc(ex.args[3])
+  return quote
+    ((tensor($T1) isa CombinerTensor && tensor($T2) isa DenseTensor) || (tensor($T2) isa CombinerTensor && tensor($T1) isa DenseTensor)) ?
+    contract($T1, $T2; inplace=true) : error("One of the arguments must be a CombinerTensor and the other a DenseTensor")
+  end
 end
 
 function contraction_output(
-  T1::TensorT1, T2::TensorT2, indsR
+  ::TensorT1, denseT::TensorT2, indsR::Tuple; inplace:: Bool = false
+) where {TensorT1<:CombinerTensor,TensorT2<:DenseTensor}
+  TensorR = contraction_output_type(TensorT1, TensorT2, indsR)
+  if inplace
+    @assert dim(indsR) == dim(inds(denseT))
+    @assert TensorR isa Type{<:DenseTensor}
+    return TensorR(AllowAlias(),indsR,storage(denseT))
+  end
+  return similar(TensorR,indsR)
+end
+
+function contraction_output(
+  T1::TensorT1, T2::TensorT2, indsR; kwargs...
 ) where {TensorT1<:DenseTensor,TensorT2<:CombinerTensor}
-  return contraction_output(T2, T1, indsR)
+  return contraction_output(T2, T1, indsR; kwargs...)
 end
 
 function contract!!(
@@ -17,7 +49,7 @@ function contract!!(
   combiner_tensor::CombinerTensor,
   combiner_tensor_labels,
   tensor::Tensor,
-  tensor_labels,
+  tensor_labels; inplace::Bool = false
 )
   if ndims(combiner_tensor) ≤ 1
     # Empty combiner, acts as multiplying by 1
@@ -30,7 +62,7 @@ function contract!!(
     ui = setdiff(combiner_tensor_labels, tensor_labels)[]
     newind = inds(combiner_tensor)[findfirst(==(ui), combiner_tensor_labels)]
     cpos1, cpos2 = intersect_positions(combiner_tensor_labels, tensor_labels)
-    output_tensor_storage = copy(storage(tensor))
+    output_tensor_storage = inplace ? storage(tensor) : copy(storage(tensor))
     output_tensor_inds = setindex(inds(tensor), newind, cpos2)
     return NDTensors.tensor(output_tensor_storage, output_tensor_inds)
   end
@@ -60,10 +92,11 @@ function contract!!(
     perm = getperm(labels_perm, tensor_labels)
     tensorp = reshape(output_tensor, permute(inds(tensor), perm))
     permutedims!(tensorp, tensor, perm)
-    return reshape(tensorp, output_tensor_inds)
+    res = reshape(tensorp, output_tensor_inds)
+    return res
   else # Uncombining
     cpos1, cpos2 = intersect_positions(combiner_tensor_labels, tensor_labels)
-    output_tensor_storage = copy(storage(tensor))
+    output_tensor_storage = inplace ? storage(tensor) : copy(storage(tensor)) 
     indsC = deleteat(inds(combiner_tensor), cpos1)
     output_tensor_inds = insertat(inds(tensor), indsC, cpos2)
     return NDTensors.tensor(output_tensor_storage, output_tensor_inds)
@@ -79,7 +112,7 @@ function contract!!(
   tensor::Tensor,
   tensor_labels,
   combiner_tensor::CombinerTensor,
-  combiner_tensor_labels,
+  combiner_tensor_labels; kwargs...
 )
   return contract!!(
     output_tensor,
@@ -87,7 +120,7 @@ function contract!!(
     combiner_tensor,
     combiner_tensor_labels,
     tensor,
-    tensor_labels,
+    tensor_labels; kwargs...
   )
 end
 
@@ -95,9 +128,9 @@ function contract(
   diag_tensor::DiagTensor,
   diag_tensor_labels,
   combiner_tensor::CombinerTensor,
-  combiner_tensor_labels,
+  combiner_tensor_labels; kwargs...
 )
   return contract(
-    dense(diag_tensor), diag_tensor_labels, combiner_tensor, combiner_tensor_labels
+    dense(diag_tensor), diag_tensor_labels, combiner_tensor, combiner_tensor_labels; kwargs...
   )
 end
